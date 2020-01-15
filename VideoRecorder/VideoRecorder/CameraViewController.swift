cc09a84afba567a5249138772d65987522249f26
Dan M <djmthedev@gmail.com>
2020-01-15 09:46:09 -0800

    set up a capture device

--- VideoRecorder/VideoRecorder/CameraViewController.swift
+++ VideoRecorder/VideoRecorder/CameraViewController.swift
@@ -7,25 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
+    
+    // MARK: - Outlets
 
     @IBOutlet var recordButton: UIButton!
     @IBOutlet var cameraView: CameraPreviewView!
 
+    // MARK: - Properties
+    
+    lazy private var captureSession = AVCaptureSession()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		// Resize camera preview to fill the entire screen
 		cameraView.videoPlayerView.videoGravity = .resizeAspectFill
-	}
-
+        
+        setUpCamera()
+    }
 
     @IBAction func recordButtonPressed(_ sender: Any) {
+        
+        
 
         
 	}
+    
+    // MARK: - Methods
+    
+    private func setUpCamera() {
+        /// get the best camera
+        let camera = bestCamera()
+    }
+    
+    /// WideAngle Lens
+    private func bestCamera() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+            return device
+        }
+        // Fallback camera
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return device
+        }
+        
+        fatalError("No cameras on the device. Or you are running on the Simulator (not supported)")
+    }
+    
+    
 	
 	/// Creates a new file URL in the documents directory
 	private func newRecordingURL() -> URL {
